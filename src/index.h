bd302928147df7adbdd1339a2a032abbc8546b5c
wtzhuque <w.t.zhuque@gmail.com>
2018-03-28 00:10:07 +0800

    add basic files

---
+++ src/index.h
@@ -0,0 +1,11 @@
+/*********************************/
+/* Codu07                        */
+/* Email: wtzhuque@163.com       */
+/*********************************/
+
+namespace codu {
+
+class Index {
+}; // class Index
+
+} // namespace codu
